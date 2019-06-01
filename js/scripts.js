ad79ff4f5031fd9bb2b199648d762bba4ce4a206
macymuhia <mercy8muhia@gmail.com>
2019-06-01 23:36:44 +0300

    refactor javascript code from one big function to 3 functions performing specific tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,38 @@
-function validateResult() {
-    var response = [];
+var response = [];
 
+function validateResult() {
     var firstSide = parseFloat(document.getElementById("first-side").value);
     var secondSide = parseFloat(document.getElementById("second-side").value);
     var thirdSide = parseFloat(document.getElementById("third-side").value);
 
     if (firstSide && secondSide && thirdSide) {
         response.push(firstSide, secondSide, thirdSide);
-        if (response[0] === response[1] &&
-            response[1] === response[2] &&
-            response[2] === response[0]) {
-            alert("you have an equilateral triangle");
-        } else if (response[0] === response[1] &&
-            response[1] !== response[2]) {
-            alert("You have an isosceles triangle");
-        } else if (response[0] !== response[1] &&
-            response[1] === response[2]) {
-            alert("You have an isosceles triangle");
-        } else if (response[0] !== response[1] &&
-            response[2] === response[0]) {
-            alert("You have an isosceles triangle");
-        } else {
-            alert("Those dimentions cannot form a triangle");
-        }
+        dimensionLogic();
+
     } else {
         alert("Kindly input three dimensions");
     }
 }
 
+function triangleName() {
+    if (response[0] === response[1] &&
+        response[1] === response[2]) {
+        alert("You have an equilateral triangle");
+    } else if ((response[0] === response[1] && response[1] !== response[2]) ||
+        (response[0] !== response[1] && response[1] === response[2]) ||
+        (response[0] !== response[1] && response[2] === response[0])) {
+        alert("You have an isosceles triangle");
+    } else {
+        alert("You have a scarlene triangle");
+    }
+
+    response = [];
+
+}
+
 function dimensionLogic() {
-    if (response[0] + response[1] > response[2] || response[1] + response[2] > response[0] || response[2] + response[0] > response[1]) {
-        alert("Those dimentions cannot form a triangle");
+    if (response[0] + response[1] <= response[2] || response[1] + response[2] <= response[0] || response[2] + response[0] <= response[1]) {
+        alert("Those dimensions cannot form a triangle");
     } else {
         triangleName();
     }

4ab93c3e897e3cd7e1edd6214b5728e31a6a2ccd
macymuhia <mercy8muhia@gmail.com>
2019-06-01 18:34:35 +0300

    Add functionality to identify an isosceles triangle

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,17 @@ function validateResult() {
             response[1] === response[2] &&
             response[2] === response[0]) {
             alert("you have an equilateral triangle");
+        } else if (response[0] === response[1] &&
+            response[1] !== response[2]) {
+            alert("You have an isosceles triangle");
+        } else if (response[0] !== response[1] &&
+            response[1] === response[2]) {
+            alert("You have an isosceles triangle");
+        } else if (response[0] !== response[1] &&
+            response[2] === response[0]) {
+            alert("You have an isosceles triangle");
         } else {
-            alert("Not an equilateral triangle");
+            alert("Not an equilateral or isosceles triangle");
         }
     } else {
         alert("Kindly add three dimensions");

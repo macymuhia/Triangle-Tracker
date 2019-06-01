9dff5876625db07f3eaa7d0a0e000a3dcb0fe8b5
macymuhia <mercy8muhia@gmail.com>
2019-06-01 17:05:39 +0300

    Add a javascript function to get user input and store in an array

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function result() {
+    var firstSide = document.getElementById("first-side").value;
+    var secondSide = document.getElementById("second-side").value;
+    var thirdSide = document.getElementById("third-side").value;
+
+    var response = [firstSide, secondSide, thirdSide];
+
+    alert(response);
+}

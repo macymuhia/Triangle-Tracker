d0404cb0f3159c3bd223da12afadcad8c706faf2
macymuhia <mercy8muhia@gmail.com>
2019-06-01 17:55:18 +0300

    Add functionality to identify an equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 function validateResult() {
+    var response = [];
+
     var firstSide = parseFloat(document.getElementById("first-side").value);
     var secondSide = parseFloat(document.getElementById("second-side").value);
     var thirdSide = parseFloat(document.getElementById("third-side").value);
 
-    var response = [];
-
     if (firstSide && secondSide && thirdSide) {
         response.push(firstSide, secondSide, thirdSide);
-        alert(response);
+        if (response[0] === response[1] &&
+            response[1] === response[2] &&
+            response[2] === response[0]) {
+            alert("you have an equilateral triangle");
+        } else {
+            alert("Not an equilateral triangle");
+        }
     } else {
         alert("Kindly add three dimensions");
     }
